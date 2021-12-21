2f2678ba5a9661ab5a0bbc9899efa67f313ee5db
Uladzislau Arlouski <uladzislau_arlouski@epam.com>
2021-12-21 19:33:53 +0300

    Adjust security policy for CONNECT STOMP command

--- src/main/java/by/andrus/askit/config/WsSecurityConfig.java
+++ src/main/java/by/andrus/askit/config/WsSecurityConfig.java
@@ -5,21 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
-import static org.springframework.messaging.simp.SimpMessageType.MESSAGE;
-import static org.springframework.messaging.simp.SimpMessageType.SUBSCRIBE;
+import static org.springframework.messaging.simp.SimpMessageType.CONNECT;
 
 @Configuration
 public class WsSecurityConfig
         extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-        messages
-                .nullDestMatcher().authenticated()
-                .simpSubscribeDestMatchers("/topic/**").hasAnyRole(Roles.ADMIN.toString(), Roles.USER.toString())
+        messages.simpSubscribeDestMatchers("/topic/**").hasAnyRole(Roles.ADMIN.toString(), Roles.USER.toString())
                 .simpDestMatchers("/app/**").hasAnyRole(Roles.ADMIN.toString(), Roles.USER.toString())
-                .simpTypeMatchers(MESSAGE, SUBSCRIBE).denyAll()
-//                .anyMessage().denyAll()
-        ;
+                .simpTypeMatchers(CONNECT).permitAll()
+                .nullDestMatcher().authenticated();
     }
 
     @Override
