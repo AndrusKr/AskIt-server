96e71064d76cc5e2dbc306dd3c89769961b7346f
akryvichenko <akryvichenko@hubspot.com>
2022-01-01 19:32:00 +0300

    Updated ER diagram

--- src/main/resources/db/migration/V3__Create_questions_table.sql
+++ src/main/resources/db/migration/V3__Create_questions_table.sql
@@ -2,9 +2,10 @@ CREATE TABLE questions
 (
     id             uuid PRIMARY KEY            NOT NULL,
     text           VARCHAR(255)                NOT NULL,
+    is_pinned      BOOLEAN                     NOT NULL,
     answer_time    TIMESTAMP WITHOUT TIME ZONE NULL,
     ask_time       TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     last_edited_at TIMESTAMP WITHOUT TIME ZONE NULL,
-    author_id        BIGINT                      NOT NULL,
+    author_id      BIGINT                      NOT NULL,
     FOREIGN KEY (author_id) REFERENCES users (id)
 );
