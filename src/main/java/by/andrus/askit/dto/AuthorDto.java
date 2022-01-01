2a47cf36e334dac73a2b43a468883f55e17f6088
akryvichenko <akryvichenko@hubspot.com>
2022-01-01 19:33:26 +0300

    Changed AuthorDto to have more precise field names to match the client

--- src/main/java/by/andrus/askit/dto/AuthorDto.java
+++ src/main/java/by/andrus/askit/dto/AuthorDto.java
@@ -1,11 +1,12 @@
 package by.andrus.askit.dto;
 
-public class AuthorDto {
-    public String id;
-    public String nickname;
 
-    public AuthorDto(Long id, String nickname) {
-        this.id = id.toString();
-        this.nickname = nickname;
+public class AuthorDto {
+    public String authorId;
+    public String authorNickname;
+
+    public AuthorDto(Long authorId, String authorNickname) {
+        this.authorId = authorId.toString();
+        this.authorNickname = authorNickname;
     }
 }
